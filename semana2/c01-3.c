21a488633d2ac9436e50b8a1a976746d2d6d3f6e
FrigusIgnis <araujo.silva.nicolas@gmail.com>
2022-09-28 00:17:51 +0000

    Added exercise 35

--- semana2/c01-3.c
+++ semana2/c01-3.c
@@ -3,7 +3,21 @@
 
 // 35, 37, 39, 40, 43, 44, 46, 47, 48, 49, 51, 53
 
+// Exercício 35
+
+float hypotenuse() {
+  float a, b, hypo;
+
+  printf("Digite os valores os catetos: ");
+  scanf("%f %f", &a, &b);
+
+  hypo = sqrt((a * a) + (b * b));
+  
+  return hypo;
+}
+
 int main() {
+    // printf("Hipotenusa: %.2f", hypotenuse()); // Exercício 35
 
     return 0;
 }
